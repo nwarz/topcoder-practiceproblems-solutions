60ef4e15ee1fdfc2e0b1c37fb5e8c10fc8bcc839
Nick Warzinski <nwarz@users.noreply.github.com>
2018-02-03 14:06:24 -0500

    cleaned up tests

--- src/test/java/ABTest.java
+++ src/test/java/ABTest.java
@@ -3,50 +3,25 @@ import org.junit.jupiter.api.Test;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ABTest {
 
-    /*
-    You are given two s: N and K. Lun the dog is interested in strings that satisfy the following conditions:
-
-         The string has exactly N characters, each of which is either 'A' or 'B'.
-         The string s has exactly K pairs (i, j) (0 <= i < j <= N-1) such that s[i] = 'A' and s[j] = 'B'.
-
-    If there exists a string that satisfies the conditions, find and return any such string. Otherwise, return an empty string.
-     */
-//    @Test
-//    public void testCreateString() {
-//
-//        {
-//            int n = 3;
-//            int k = 2;
-//            String result = AB.createString(n,k);
-//            assertTrue(lengthIsCorrect(result,n));
-//            assertEquals("ABB", result);
-//        }
-//        assertEquals("BA", AB.createString(2,0));
-//        assertEquals("", AB.createString(5,8));
-//        assertEquals()
-//    }
-//
-//    private boolean lengthIsCorrect(String string, int n) {
-//        return string.length() == n;
-//    }
-
     @Test
     public void testCountPairs() {
-        assertEquals(1,AB.countPairs("AB"));
-        assertEquals(0, AB.countPairs("AAAA"));
-        assertEquals(3, AB.countPairs("AAAB"));
-        assertEquals(2, AB.countPairs("AABA"));
-        assertEquals(4, AB.countPairs("AABB"));
-        assertEquals(1, AB.countPairs("ABAA"));
-        assertEquals(2, AB.countPairs("BAAB"));
-        assertEquals(2, AB.countPairs("BABBA"));
-        assertEquals(5, AB.countPairs("ABABB"));
-        assertEquals(6, AB.countPairs("AABBB"));
+        assertAll(
+            () -> assertEquals(1,AB.countPairs("AB")),
+            () -> assertEquals(0, AB.countPairs("AAAA")),
+            () -> assertEquals(3, AB.countPairs("AAAB")),
+            () -> assertEquals(2, AB.countPairs("AABA")),
+            () -> assertEquals(4, AB.countPairs("AABB")),
+            () -> assertEquals(1, AB.countPairs("ABAA")),
+            () -> assertEquals(2, AB.countPairs("BAAB")),
+            () -> assertEquals(2, AB.countPairs("BABBA")),
+            () -> assertEquals(5, AB.countPairs("ABABB")),
+            () -> assertEquals(6, AB.countPairs("AABBB")));
     }
 
     @Test
@@ -69,43 +44,14 @@ public class ABTest {
 
     @Test
     public void testKAtLeastNCreateString() {
-        String result;
-        {
-            result = AB.createString(30,150);
-            System.out.println(result);
-            assertEquals(45, AB.countPairs(result));
-        }
+        assertEquals(150, AB.countPairs(AB.createString(30,150)));
     }
 
     @Test
     public void testKTooLargeCreateString() {
-        String result = AB.createString(5,8);
-        assertEquals("", result);
-
-        result = AB.createString(11,30);
-        assertEquals(30, AB.countPairs(result));
+        assertAll(
+            () -> assertEquals("", AB.createString(5,8)),
+            () -> assertEquals(30, AB.countPairs(AB.createString(11,30))));
     }
 
-    @Test
-    public void testNKPairExhaustively() {
-        int n = 11;
-        int k = 30;
-        StringBuilder stringBuilder = new StringBuilder();
-
-        for(int i = 0; i < (Math.pow(2,n)); ++i) {
-            stringBuilder.setLength(0);
-            for(int j = 0; j<n; j++) {
-                if((i >> j)%2 == 0) {
-                    stringBuilder.append("A");
-                } else {
-                    stringBuilder.append("B");
-                }
-            }
-            String result = stringBuilder.toString();
-            int pairs = AB.countPairs(result);
-            if(pairs == k) {
-                System.out.println(result + " - " + pairs);
-            }
-        }
-    }
 }
--- src/test/java/SubstituteTest.java
+++ src/test/java/SubstituteTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
@@ -7,26 +8,9 @@ public class SubstituteTest {
 
     @Test
     void testGetValue() {
-        /**
-            key: "TRADINGFEW"
-            code: "LGXWEV"
-            Returns: 709
-         */
-        assertEquals(709, Substitute.getValue("TRADINGFEW", "LGXWEV"));
-
-        /**
-            "ABCDEFGHIJ"
-            "XJ"
-            Returns: 0
-         */
-        assertEquals(0, Substitute.getValue("ABCDEFGHIJ", "XJ"));
-
-        /**
-            "CRYSTALBUM"
-            "MMA"
-            Returns: 6
-         */
-        assertEquals(6, Substitute.getValue("CRYSTALBUM", "MMA"));
-
+        assertAll(
+                () -> assertEquals(709, Substitute.getValue("TRADINGFEW", "LGXWEV")),
+                () -> assertEquals(0, Substitute.getValue("ABCDEFGHIJ", "XJ")),
+                () -> assertEquals(6, Substitute.getValue("CRYSTALBUM", "MMA")));
     }
 }
